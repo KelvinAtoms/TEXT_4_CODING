594a6862ca0f545e5b9391eba9a149fa735090ee
Kelvin <146788788+KelvinAtoms@users.noreply.github.com>
2024-12-20 21:22:56 +0700

    Add files via upload

---
+++ lmao-2311COMP180201-BTNHOM-5AE/lmao-2311COMP180201-BTNHOM-5AE/Find-The-Shortest-Cost-Path-With-Dijkstra-Algorithm/Find-The-Shortest-Cost-Path-With-Dijkstra-Algorithm-master/District.cs
@@ -0,0 +1,40 @@
+﻿using Dijkstra_Vietnam;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dijkstra_Vietnam
+{
+    public class District
+    {
+        private string nameDistrict { get; set; }
+        private string pointName { get; set; }
+        private Point districtLocation { get; set; }
+
+        public District(string name, string symbol, int x, int y)
+        {
+            nameDistrict = name;
+            pointName = symbol;
+            Point p = new Point(x, y);
+            districtLocation = p;
+        }
+        public string getName()
+        {
+            return nameDistrict;
+        }
+        public string getPointName()
+        {
+            return pointName;
+        }
+        public Point getPoint()
+        {
+            return districtLocation;
+        }
+    }
+}
